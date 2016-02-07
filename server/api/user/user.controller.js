cb386868efe232f9e4f8cb1ecfcd0ef1666068c2
Christopher Mehmed <chrismehmed@gmail.com>
2016-02-07 12:57:52 -0500

    admin delete user

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -44,7 +44,7 @@ export function index(req, res) {
 export function create(req, res, next) {
   var newUser = new User(req.body);
   newUser.provider = 'local';
-  newUser.role = 'admin';
+  newUser.role = 'user';
   newUser.saveAsync()
     .spread(function(user) {
       var token = jwt.sign({ _id: user._id }, config.secrets.session, {
